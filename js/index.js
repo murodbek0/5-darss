0cec6b3981d66ec116c4d8ad37f2f08590238e8a
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-07-25 00:39:56 +0500

    5 dars

---
+++ js/index.js
@@ -0,0 +1,208 @@
+// 1 misol
+//  let arr = [2,4,6,8,10];
+//  let res= [];
+// for(let i = 0; i < arr.length; i++){
+//    res.push(arr[i]*2);
+
+// }
+// console.log(res);
+
+// 2 misol
+//  let arr = [2,4,6,8,10];
+//  let res= [];
+// for(let i = 0; i < arr.length; i++){
+//    res.unshift(arr[i]);
+
+// }
+// console.log(res);
+
+// 3 misol
+
+//  let arr = [2,-4,6,8,-10];
+//  let res= [];
+// for(let i = 0; i < arr.length; i++){
+//  if(arr[i] > 0){
+//  res.push(arr[i])
+
+// }
+
+// }
+// console.log(res);
+
+// 4 misol
+
+//  let arr = [2,4,6,8,10];
+//  let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+//    sum = sum + arr[i];
+// }
+// console.log(sum);
+
+// 5 misol
+
+// let name = ['aziz','ozod','islom',1,2,3];
+// name.push('salom');
+// console.log(name);
+
+// 6 misol
+
+// let name = ['aziz','ozod','islom',1,2,3];
+// name.pop();
+// console.log(name);
+
+// 7 misol
+
+// let name = ['aziz','ozod','islom',1,2,3];
+// name.shift();
+// console.log(name);
+
+// 8 misol
+
+// let name = ['aziz','ozod','islom',1,2,3];
+// name.unshift('murodbek');
+// console.log(name);
+
+// 9 misol
+
+// let arr = ['aziz','ozod','islom',1,2,3];
+// console.log(arr[5]);
+
+// 10 misol
+
+// let arr = ['aziz','ozod','islom',1,2,3];
+// let res = [];
+// res.push(arr);
+// console.log(res)
+
+// 11 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let max = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+
+// 12 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let min = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
+
+// 13 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let res = [];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     res.push(arr[i])
+//   }
+// }
+// console.log(res);
+
+// 14 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let res = [];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] % 2 == 1) {
+//     res.push(arr[i])
+//   }
+// }
+// console.log(res);
+
+// 15 misol
+
+// let arr = [1,0, 2,0, 3,0, 4, 5, 6, 7];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] !== 0) {
+//     res.push(arr[i])
+//   }
+// }
+// console.log(res);
+
+// 16 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let res = [];
+// for (let i = 1; i < arr.length; i++) {
+//     res.push(arr[i]);
+//     res.push(arr[i]);
+// }
+// console.log(res);
+
+// 17 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let res = [];
+// let res1 = [];
+// for (let i = 1; i < arr.length; i++) {
+//     res.push(arr[i]);
+//     res1.push(arr[i]);
+// }
+// console.log(res);
+
+// 18 misol
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let sum = 1;
+// for (let i = 0; i < arr.length; i++) {
+//     sum = sum * arr[1];
+
+// }
+// console.log(sum);
+
+// 19 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let res = [];
+// for (let i = 0; i < arr.length; i+=2) {
+//     res.push(arr[i])
+// }
+// console.log(res);
+
+// 20 misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let res = [];
+// for (let i = 1; i < arr.length; i+= 2) {
+//     res.push(arr[i])
+// }
+// console.log(res);
+
+// 21 misol
+// let arr = [1, 'salom', 3, 'qale', 5, 'hayrli', 7];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if(typeof arr[i] === 'string'){
+//       res.push(arr[i]);
+
+//     }
+// }
+// console.log(res);
+
+// 22 misol
+
+// let arr = [1, "salom", 3, "qale", 5, "hayrli", 7];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (typeof arr[i] === 'number') {
+//     res.push(arr[i]);
+//   }
+// }
+// console.log(res);
+
+// 23 misol
+
+// let arr = [1, "salom", 3, "qale", 5, "hayrli", 7];
+// let length = arr.length
+// console.log(length);
+
